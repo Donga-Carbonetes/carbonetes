cd7e26c776617dd5204425d76fa23f8470d949c5
채승지 <cotmdwl1405@gmail.com>
2025-04-07 22:18:30 +0900

    feat : multi cluster 접속 및 파드 배포 테스트

--- job-queue/consumer.py
+++ job-queue/consumer.py
@@ -4,20 +4,22 @@ import subprocess
 import time
 
 SERVER_URL = "http://localhost:5000"
-
+contexts = ['default', 'docker-desktop']
 def run_job(job):
     try:
         if job["type"] == "python_script":
             cmd = ["python", job["script"]] + job["args"]
         elif job["type"] == "kubectl_cmd":
-            cmd = [job["script"]] + job["args"]  # 예: ["kubectl", "get", "nodes"]
+            cmd = job["script"]  # 예: ["kubectl", "get", "nodes"]
         else:
             print(f"⚠️ 알 수 없는 작업 타입: {job['type']}")
             requests.post(f"{SERVER_URL}/jobs/{job['id']}/status", json={"status": "FAILED"})
             return
 
-        print(f"▶️ 실행 명령어: {' '.join(cmd)}")
-        subprocess.run(cmd, check=True)
+        for context in contexts:
+            print(f"▶️ 실행 명령어: {' '.join(cmd)}")
+            subprocess.run(f'kubectl config use-context {context}', check=True)
+            subprocess.run(cmd, check=True)
 
         requests.post(f"{SERVER_URL}/jobs/{job['id']}/status", json={"status": "DONE"})
         print(f"✅ 작업 완료: {job['id']}")
