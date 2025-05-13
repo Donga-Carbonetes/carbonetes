e440f07262bec787618d49db47b314a8aee435d3
rzl0zi <program.yunji@gmail.com>
2025-05-13 17:52:36 +0900

    feat : db연동

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require("sequelize");
+require("dotenv").config(); // ⬅️ .env 읽기
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,     // DB 이름
+  process.env.DB_USER,     // 사용자
+  process.env.DB_PASSWORD, // 비밀번호
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    logging: false, // SQL 로그 보기 싫으면 false
+  }
+);
+
+module.exports = sequelize;
