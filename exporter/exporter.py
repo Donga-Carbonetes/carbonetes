e59e3b5cedde7b8f7c318951ebc3dd449579cea2
채승지 <cotmdwl1405@gmail.com>
2025-05-15 21:46:51 +0900

    feat: minio에 파일 업로드 하는 기능 추가

--- exporter/exporter.py
+++ exporter/exporter.py
@@ -8,7 +8,10 @@ import time
 import os
 from dotenv import load_dotenv
 import mysql.connector
-import uuid
+import ast
+from minio import Minio
+from minio.error import S3Error
+
 
 def load_user_module(path="/mnt/main.py"):
     spec = importlib.util.spec_from_file_location("user_module", path)
@@ -16,11 +19,15 @@ def load_user_module(path="/mnt/main.py"):
     spec.loader.exec_module(user_module)
     return user_module
 
-def make_task_name_with_UUID(model_name: str) -> str:
-    unique_id = uuid.uuid4().hex
-    return f"task-{model_name}-{unique_id}"
+def export_data(result):
+    if result:
+        raw_shape, dataset_size, label_count = result
+        data_shape = ast.literal_eval(raw_shape)
+        return (dataset_size, data_shape, label_count)
+    return None
 
-def extract_training_profile(model, optimizer, loss_fn, batch_size, epochs, device):
+
+def extract_training_profile(model, optimizer, loss_fn, batch_size, epochs, device, dataset_size, data_shape, label_count):
     model_info = {
         "model_name": model.__class__.__name__
     }
@@ -35,9 +42,9 @@ def extract_training_profile(model, optimizer, loss_fn, batch_size, epochs, devi
     }
 
     dataset_info = {
-        "dataset_size": 50000,
-        "data_shape": [3, 32, 32],
-        "label_count": 10
+        "dataset_size": dataset_size,
+        "data_shape": data_shape,
+        "label_count": label_count
     }
 
     profile = {
@@ -80,12 +87,68 @@ def profiling(profile, model, optimizer, loss_function):
     steps = profile['dataset']['dataset_size'] // batch
     return (end_time - start_time) * steps
 
+def upload_file_minio(local_file_path, task_name):
+    minio_host = os.getenv("MINIO_HOST")
+    minio_port = os.getenv("MINIO_PORT")
+    minio_user = os.getenv("MINIO_USER")
+    minio_password = os.getenv("MINIO_PASSWORD")
+
+    host = f"{minio_host}:{minio_port}"
+    client = Minio(
+    host,
+    access_key=minio_user,
+    secret_key=minio_password,
+    secure=False
+    )
+
+    bucket_name = "mybucket"
+    object_name = f"{task_name}.py"
+    
+    if not client.bucket_exists(bucket_name):
+        client.make_bucket(bucket_name)
+    
+    try:
+        client.fput_object(bucket_name, object_name, local_file_path)
+        print(f"✅ '{object_name}' 업로드 성공")
+    except S3Error as e:
+        print(f"❌ 업로드 실패: {e}")
+
 
 if __name__ == "__main__":
     load_dotenv()
-    user_module = load_user_module("/mnt/main.py") # for docker
-    # user_module = load_user_module("E:\carbonetes\exporter\sample_resnet.py") # for local
+    task_name = os.getenv("TASK_NAME")
 
+    local_path = f"/mnt/{task_name}.py" # for docker 
+    # local_path = "E:\carbonetes\exporter\sample_resnet.py" # for local test
+
+    user_module = load_user_module(local_path) 
+    
+
+        # DB 연결
+    db_config = {
+        "host": os.getenv("MYSQL_HOST"),
+        "port": int(os.getenv("MYSQL_PORT")),
+        "user": os.getenv("MYSQL_USER"),
+        "password": os.getenv("MYSQL_PASSWORD"),
+        "database": os.getenv("MYSQL_DATABASE")
+    }
+
+    dataset_size = None
+    data_shape = None
+    label_count = None
+
+
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+    query = """
+    SELECT data_shape, dataset_size, label_count
+    FROM task_info
+    WHERE task_name = %s
+    """
+    cursor.execute(query, (task_name,))
+    result = cursor.fetchone()
+    dataset_size, data_shape, label_count = export_data(result)
+    
     # 추출
     profile = extract_training_profile(
         model=user_module.network,
@@ -93,7 +156,10 @@ if __name__ == "__main__":
         loss_fn=user_module.loss_function,
         batch_size=user_module.batch_size,
         epochs=user_module.training_epochs,
-        device=user_module.device if hasattr(user_module, "device") else ("cuda" if torch.cuda.is_available() else "cpu")
+        device=user_module.device if hasattr(user_module, "device") else ("cuda" if torch.cuda.is_available() else "cpu"),
+        dataset_size=dataset_size,
+        data_shape=data_shape,
+        label_count=label_count
     )
 
     pprint.pprint(profile)
@@ -107,33 +173,21 @@ if __name__ == "__main__":
 
     standard_time = times[5]
 
-    # DB 연결
-    db_config = {
-        "host": os.getenv("MYSQL_HOST"),
-        "port": int(os.getenv("MYSQL_PORT")),
-        "user": os.getenv("MYSQL_USER"),
-        "password": os.getenv("MYSQL_PASSWORD"),
-        "database": os.getenv("MYSQL_DATABASE")
-    }
-
-    conn = mysql.connector.connect(**db_config)
-    cursor = conn.cursor()
-
-    insert_query = """
-    INSERT INTO task_info (task_name, data_shape, dataset_size, label_count, estimated_time)
-    VALUES (%s, %s, %s, %s, %s)
+    update_query = """
+    UPDATE task_info SET estimated_time=%s, status='ready' WHERE task_name=%s
     """
 
-    task_name = make_task_name_with_UUID(profile['model']['model_name'])
-    data_shape = str(profile['dataset']['data_shape'])
-    dataset_size = profile['dataset']['dataset_size']
-    label_count = profile['dataset']['label_count']
     estimated_time = standard_time * profile['hyperparameters']['epochs']
 
-    cursor.execute(insert_query, (task_name, data_shape, dataset_size, label_count, estimated_time))
+    cursor.execute(update_query, (estimated_time, task_name))
     conn.commit()
 
-    print(f"✅ DB에 task 등록 완료! ID: {cursor.lastrowid}")
+    print(f"✅ DB에 task 업데이트 완료!")
 
     cursor.close()
     conn.close()
+
+    # minio에 파일 업로드하기
+    upload_file_minio(local_path, task_name)
+
+    # 끝나고 스케줄러에 새로운 작업이 생겼다고 알려주기
