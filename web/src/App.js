3834d321b74443b0640a1eb87a2f0b45ae517438
채승지 <cotmdwl1405@gmail.com>
2025-05-19 22:17:09 +0900

    chore: 환경변수로부터 백엔드 주소 받아오도록 코드 수정

--- web/src/App.js
+++ web/src/App.js
@@ -5,7 +5,9 @@ import SubmitForm from "./components/SubmitForm"
 import TaskMonitor from "./components/TaskMonitor"
 import "./App.css"
 // const API_URL = "http://localhost:4000/api/tasks"; // 백엔드 주소
-const API_URL = "http://carbon-backend:4000/api/tasks"; // 백엔드 주소
+const HOST = process.env.BACKEND_HOST
+const PORT = process.env.BACKEND_PORT
+const API_URL = f`http://${HOST}:${PORT}/api/tasks`; // 백엔드 주소
 
 function App() {
   const [activeTab, setActiveTab] = useState("submit")
