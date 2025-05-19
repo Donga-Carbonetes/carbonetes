27c8c20df909f3b3c0fa10b48e5b5ec94f32177a
채승지 <cotmdwl1405@gmail.com>
2025-05-19 22:36:15 +0900

    chore: 백엔드 주소 환경변수로 가져오기

--- web/src/App.js
+++ web/src/App.js
@@ -7,7 +7,7 @@ import "./App.css"
 // const API_URL = "http://localhost:4000/api/tasks"; // 백엔드 주소
 const HOST = process.env.BACKEND_HOST
 const PORT = process.env.BACKEND_PORT
-const API_URL = f`http://${HOST}:${PORT}/api/tasks`; // 백엔드 주소
+const API_URL = `http://${HOST}:${PORT}/api/tasks`; // 백엔드 주소
 
 function App() {
   const [activeTab, setActiveTab] = useState("submit")
