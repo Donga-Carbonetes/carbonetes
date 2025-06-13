eab26ffbd2b5b9499d2109282161567268339c0a
Jooyoung Jeong <72073045+zzooyoung@users.noreply.github.com>
2025-06-13 19:38:38 +0900

    feat : TastCase 0 초안 작성

--- scheduler/task_processor.py
+++ scheduler/task_processor.py
@@ -1,15 +1,15 @@
 import logging
 import time
 import os
-import sys
-import math
 import csv
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
-import random
 import mysql.connector
 from carbon_collector.carbon_fetch_model import get_carbon_info
 from resource_collector.new_collector import get_resource_usage
+
+
+load_dotenv()
 
 db_config = {
     "host": os.getenv("MYSQL_HOST"),
@@ -19,21 +19,18 @@ db_config = {
     "database": "carbonetes"
 }
 
-load_dotenv()
 
 # kluster_nodes = ["K3S1", "K3S2", "NEWK3S1"]
 # kluster_region = ["KR", "JP", "FR"]
 # kluster_name = ["k3s-1", "k3s-2", "new-k3s-1"]
 # endpoint = []
 
-csv_file = "task_log.csv"
+csv_file = "task_case0_log.csv"
 if not os.path.exists(csv_file):
     with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         writer.writerow([
-            "timestamp", "task_name", "node", "resource_usage_avg",
-            "active_node_count", "penalty", "workspan",
-            "carbon_emission", "score"
+            "timestamp", "task_name", "node", "resource_usage_avg"
         ])
 
 class Node:
@@ -41,19 +38,6 @@ class Node:
         self.cluster_name = cluster_name
         self.cluster_ip = cluster_ip
         self.region = region
-        self.expected_finish_at = None
-
-    def get_remaining_time(self):
-        if self.expected_finish_at is None:
-            return 0
-        remaining = (self.expected_finish_at - datetime.now()).total_seconds()
-        return max(0, remaining)
-
-    def assign_task(self, task_duration):
-        now = datetime.now()
-        remaining = self.get_remaining_time()
-        self.expected_finish_at = now + timedelta(seconds=remaining + task_duration)
-        print(f"✅ {self.cluster_name} - 종료 예정: {self.expected_finish_at}")
 
 def get_cluster_info_from_db():
     conn = None
@@ -99,52 +83,31 @@ def process_task(task_name, estimated_time):
                     cpu, ram = get_resource_usage(f'{endpoint}:9100')
                     retry += 1
                 usage = cpu
-                carbon_info = get_carbon_info(estimated_time, country_code=node_obj.region)
-                carbon_emission = carbon_info.get("integratedEmission", 0)
-                remaining = node_obj.get_remaining_time()
 
                 processed_nodes_data.append({
                     "node_obj": node_obj,
                     "usage": usage,
-                    "carbon": carbon_emission,
-                    "remaining_time": remaining
                 })
 
             for idx, data in enumerate(processed_nodes_data):
                 node_obj = data["node_obj"]
                 usage = data["usage"]
-                carbon = data["carbon"]
-                remaining = data["remaining_time"]
 
                 if usage > 60:
                     with open(csv_file, mode='a', newline='', encoding='utf-8') as file:
                         writer = csv.writer(file)
                         writer.writerow([
-                            time.strftime("%Y-%m-%d %H:%M:%S"), task_name, node_obj.cluster_name,
-                            round(usage, 2), "-", "-", "-", "-", "미배치 (리소스 초과)"
+                            time.strftime("%Y-%m-%d %H:%M:%S"), task_name, node_obj.cluster_name, round(usage, 2)
                         ])
                     result_score.append(999999)
                     continue
 
-                temp_usage = [d["usage"] for d in processed_nodes_data]
-                temp_usage[idx] = 50
-                count = sum(1 for u in temp_usage if u >= 8.5)
-                work_nodes = a_w * count
-
-                normalized = usage / 100
-                penalty = b_w * (10 ** (4 * normalized))
-                workspan = c_w * (remaining + estimated_time)
-                carbon_score_term = d_w * carbon
-
-                score = work_nodes + penalty + workspan + carbon_score_term
-                result_score.append(score)
+                result_score.append(usage)
 
                 with open(csv_file, mode='a', newline='', encoding='utf-8') as file:
                     writer = csv.writer(file)
                     writer.writerow([
-                        time.strftime("%Y-%m-%d %H:%M:%S"), task_name, node_obj.cluster_name,
-                        round(usage, 2), work_nodes, round(penalty, 2),
-                        round(workspan, 2), round(carbon, 6), round(score, 2)
+                        time.strftime("%Y-%m-%d %H:%M:%S"), task_name, node_obj.cluster_name, round(usage, 2)
                     ])
 
             logging.info(f"스코어 목록: {result_score}")
@@ -157,11 +120,9 @@ def process_task(task_name, estimated_time):
 
             if valid_score:
                 if best_node_name in valid_score:
-                    nodes[best_node_name].assign_task(estimated_time)
                     return best_node_name
                 else:
                     fallback_node_name = min(valid_score, key=valid_score.get)
-                    nodes[fallback_node_name].assign_task(estimated_time)
                     logging.warning(f"⚠ 대체 노드 사용: {fallback_node_name}")
                     return fallback_node_name
             else:
