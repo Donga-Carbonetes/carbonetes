49732d1a54bd7d66e9a8cd67f00044c86d627d35
rzl0zi <program.yunji@gmail.com>
2025-06-13 20:24:30 +0900

    chore : 포트 번호 변경

--- web/src/components/TaskMonitor.js
+++ web/src/components/TaskMonitor.js
@@ -6,7 +6,7 @@ import { io } from "socket.io-client";
 
 const HOST = process.env.REACT_APP_BACKEND_HOST
 const PORT = process.env.REACT_APP_BACKEND_PORT
-const API_URL = "http://211.253.31.134:4000/api/tasks"
+const API_URL = "http://211.253.31.134:31840/api/tasks"
 function TaskMonitor() {
   const [tasks, setTasks] = useState([]);
   const [activeTab, setActiveTab] = useState("all");
