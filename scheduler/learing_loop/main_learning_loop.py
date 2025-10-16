05036475ecfc40cd36e74adeac5d2739a80edd14
DLongLegs <xoals73771@gmail.com>
2025-10-16 18:09:59 +0900

    Revert "Merge branch 'main' of https://github.com/Donga-Carbonetes/carbonetes"
    This reverts commit ccbe71e940b8a8e60cd0d9cee9593fcd30311231, reversing
    changes made to 49843c7d2825d92680e771ffd53ee6dd33782ce5.

--- scheduler/learing_loop/main_learning_loop.py
+++ scheduler/learing_loop/main_learning_loop.py
@@ -28,33 +28,24 @@ db_config = {
 
 
 # 함수 도입 부
-def learning_loop(): 
+def learning_loop(task_name, estimated_time): 
     logging.info("[학습기]학습을 시작합니다.")
     
     # [2] 로그 수집
     task_data = get_processed_tasks(db_config)
-    logging.info(task_data)
-    logging.info("[학습기] [2] 로그 수집 완료")
      
     # [3] 초기 개체군 형성
     candidates_weight = generate_candidates(n=5, include_current=True)# n 가중치 후보 갯수
-    logging.info(candidates_weight)
-    logging.info("[학습기] [3] 초기 개체군 형성 완료")
-    
+
     # [4] 시뮬레이션(재실행 가상화)
     sim_result = run_simulation_as_dicts_from_modules(task_data, candidates_weight)
-    logging.info(sim_result)
-    logging.info("[학습기] [4] 시뮬레이션 완료")
 
     # [5] 성능지표 계산(Fitness)
     best_result = calculate_and_get_best_result(sim_result, 1.00, 0.05)
     # simulation_results, alpha, gamma, use_p95_latency=True
-    logging.info(best_result)
-    logging.info("[학습기] [5] 성능지표 계산 완료")
 
     # [7] 정책 저장
     save_best_weights_to_db(best_result)
-    logging.info("[학습기] [7] 정책 저장 완료")
     logging.info(f'Learning Loop: Success Weight Save')
 
 
@@ -63,7 +54,7 @@ def save_best_weights_to_db(best_result: dict):
     가장 좋은 가중치 조합 하나를 DB에 저장합니다.
     기존 테이블 데이터를 모두 지우고 새로운 최적의 가중치만 추가합니다.
 
-    Args:a
+    Args:
         best_result (dict): 'weights'와 'custom_fitness' 키를 포함한 딕셔너리.
     """
     if not best_result:
@@ -87,6 +78,10 @@ def save_best_weights_to_db(best_result: dict):
         connection = mysql.connector.connect(**db_config)
         cursor = connection.cursor()
 
+        # 테이블을 비워서 항상 최신 값만 유지
+        cursor.execute("TRUNCATE TABLE weights")
+        print("'weights' 테이블의 기존 데이터를 삭제했습니다.")
+
         # 새로운 최적 가중치 INSERT
         sql = "UPDATE weights SET a_w = %s, b_w = %s, c_w = %s, d_w = %s"
         values = (best_weights['a'], best_weights['b'], best_weights['c'], best_weights['d'])
@@ -106,14 +101,3 @@ def save_best_weights_to_db(best_result: dict):
             cursor.close()
             connection.close()
             # print("MySQL 연결이 닫혔습니다.")
-
-
-#################################################################
-# 이 스크립트가 직접 실행될 때 learning_loop() 함수를 호출합니다.
-#################################################################
-if __name__ == "__main__":
-    # 로깅 기본 설정 (레벨, 포맷 등)
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-    
-    # 메인 학습 루프 실행
-    learning_loop()
